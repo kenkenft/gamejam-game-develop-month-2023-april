3db318975bdd42a0877c519a7021af7bbb3975b3
Ken Tang <kenftang@gmail.com>
2023-04-08 14:58:36 +0100

    Add PlayerControl class, and Player prefab
    PlayerControl has Move method. This modifies the
    Player's rigidbody velocity so that the player
    character moves in the intended direction.
    
    Player prefab created.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float _playerSpeed = 10.0f;
+    [SerializeField] private int _coinCapacity = 10;
+    private Vector2 _moveXY = new Vector2(0f, 0f);
+    public Rigidbody2D PlayerRig;
+    public Collider2D PlayerCollider;
+    
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Debug.Log("Player Move called!");
+
+        _moveXY[0] = Input.GetAxis("Horizontal") * _playerSpeed;
+        _moveXY[1] = Input.GetAxis("Vertical") * _playerSpeed;
+
+        if( _moveXY[0] !=0 || _moveXY[1] !=0)
+        {
+            PlayerRig.velocity = _moveXY;
+        }
+        
+
+    }
+}
