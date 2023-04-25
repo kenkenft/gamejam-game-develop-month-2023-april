c91bbe8f3419bdec3ad9f6ed9efad8cb7ee2b26b
Ken Tang <kenftang@gmail.com>
2023-04-25 20:26:01 +0100

    Modify UIManager
    Add CheckWhichCanvas and ToggleCanvas methods.
    CheckWhichCanvas sets the object reference for
    each canvas object, and adds it to a list,
    _canvasList.
    
    ToggleCanvas takes a string, targetCanvasName.
    It loops through _canvasList and toggles on the
    canvas that is attached to the gameObject with
    targetCanvasName. Else, it disables the gameObject.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<string, Canvas> _canvasesDict = new Dictionary<string, Canvas>(){};
+    private Canvas _playerOverlayCanvas, _titleCanvas, _pauseCanvas, _resultsCanvas;
+    private List<Canvas> _canvasList = new List<Canvas>();
+
+    void OnEnable()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        Canvas[] tempCanvasArray = GetComponentsInChildren<Canvas>();
+        foreach(Canvas canvas in tempCanvasArray)
+        {
+            CheckWhichCanvas(canvas);    
+        }
+
+        // Debug.Log(_canvasesDict.Count + "," + _canvasesDict["TitleCanvas"]);
+        Debug.Log(_canvasList.Count);
+
+        ToggleCanvas("ResultsCanvas");
     }
+
+    void CheckWhichCanvas(Canvas canvas)
+    {
+        switch(canvas.gameObject.name)
+        {
+            case "PlayerOverlayCanvas":
+            {
+                // _canvasesDict.Add("playerOverlayCanvas", canvas);
+                _playerOverlayCanvas = canvas;
+                _canvasList.Add(_playerOverlayCanvas);
+                break;
+            }
+            case "TitleCanvas":
+            {
+                // _canvasesDict.Add("titleCanvas", canvas);
+                _titleCanvas = canvas;
+                _canvasList.Add(_titleCanvas);
+                break;
+            }
+            case "PauseCanvas":
+            {
+                // _canvasesDict.Add("pauseCanvas", canvas);
+                _pauseCanvas = canvas;
+                _canvasList.Add(_pauseCanvas);
+                break;
+            }
+            case "ResultsCanvas":
+            {
+                // _canvasesDict.Add("resultsCanvas", canvas);
+                _resultsCanvas = canvas;
+                _canvasList.Add(_resultsCanvas);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+    
+
+    void ToggleCanvas(string targetCanvasName)
+    {
+        // Toggles off other canvases except target canvas
+        foreach(Canvas canvas in _canvasList)
+        {
+            if(canvas.gameObject.name == targetCanvasName)
+                canvas.gameObject.SetActive(true);
+            else
+                canvas.gameObject.SetActive(false);
+        }
+
+    }
+    //ToDo method to Toggle active state of canvas
+    //ToDo method to Toggle active state of buttons
+    //ToDo method to start game and SetUp initial game state
+    //ToDo method to show instruction text
+    //ToDo method to show credit text
+    //ToDo method to show results menu on timer expiring
+    //ToDo method to return player back to menu and clear player data
+    //ToDo method to detect pause button press
+
 }
