4a2a0dc24c2f7a2e7af3bd61f04820d906b05cf9
Ken Tang <kenftang@gmail.com>
2023-04-29 23:12:56 +0100

    Modify PlayerControl Add animation states
    Player Control now has new methods,
    SetPlayerAnimation and FlipSprite.
    
    SetPlayerAnimation will set animation parameters
    that determine which animation to play.
    
    FlipSprite mirrors the sprites x-axis when moving
    sideways.
    
    There are some issues with state transitions not
    working properly.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private float _playerSpeedBase = 10.0f, _playerSpeed, _playerPenaltyModifier = 0.4f;
+    private bool _isMovingVertical = false, _isMovingSideways = false,_isFacingRight = true;
+    // private Vector3 _playerVelocity = new Vector3(0f, 0f, 0f);
     
     private Vector2 _moveXY = new Vector2(0f, 0f);
     public Rigidbody2D PlayerRig;
+
+    public Animator animator;
 
     [HideInInspector] public delegate bool OnInteractKeyDown();
     [HideInInspector] public static OnInteractKeyDown CheckCanDeposit;
@@ -44,6 +48,7 @@ public class PlayerControl : MonoBehaviour
         if(CheckIsPlaying.Invoke())
         {
             Move();
+            SetPlayerAnimation();
             if(Input.GetKeyDown(KeyCode.E))
                 CheckInteractContext();
 
@@ -86,5 +91,42 @@ public class PlayerControl : MonoBehaviour
         Debug.Log("PlayerSpeed: " + _playerSpeed);
     }
 
-    
+    void SetPlayerAnimation()
+    {
+        // _playerVelocity = PlayerRig.velocity; 
+        
+        animator.SetFloat("_velocityY", _moveXY[1]);
+        animator.SetFloat("_velocityX", Mathf.Abs(_moveXY[0]));
+
+        if(_moveXY[1] !=0)
+            _isMovingVertical = true;
+        else
+            _isMovingVertical = false;
+        
+        if(_moveXY[0] !=0)
+            _isMovingSideways = true;
+        else
+            _isMovingSideways = false;
+        
+        if(_isMovingVertical || _isMovingSideways)
+        {    
+            animator.SetBool("_isMoving", true);
+            if(_moveXY[0] > 0f && !_isFacingRight && _isMovingSideways)
+                    FlipSprite(); 
+            else if(_moveXY[0] < 0f && _isFacingRight && _isMovingSideways) 
+                    FlipSprite();
+        }
+        else
+            animator.SetBool("_isMoving", false);
+
+        
+    }
+
+    void FlipSprite()
+    {
+        _isFacingRight = !_isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
